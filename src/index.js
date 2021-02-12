6decf8026062ea8cf198799415d5d83f3bde886b
JesusRed <jesusalfredorv@ufps.edu.co>
2021-02-12 12:06:04 -0500

    initail commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+/* import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
+import { render } from "@testing-library/react"; */
+
+let name = "Galahad";
+
+function A(props) {
+  return <p>Salut Monde! {props.name}</p>;
+}
+
+function B(props) {
+  return <p>Ciao Mondo! {props.name}</p>;
+}
+
+class MyComponentClass extends Component {
+  render() {
+    return <p>Hi, i'm from your class</p>;
+  }
+}
+
+class Appo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+  render() {
+    let name = "Galahad";
+    return (
+      <div>
+        <A name={name} />
+        <B name={name} />
+        <MyComponentClass />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Appo />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
