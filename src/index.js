42d8f4171922acff904588af03606bb55f0c3537
JesusRed <jesusalfredorv@ufps.edu.co>
2021-04-09 13:38:00 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,101 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
 import { render } from "@testing-library/react";
-
-class Blog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [
-        "Back in black",
-        "Welcome to the Jungle",
-        "Stairway to heaven",
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.articles.map((title) => {
-          return <p>{title}</p>;
-        })}
-      </div>
-    );
-  }
-}
-
-class Formulario extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
-
-  syncChanges(value, property) {
-    let state = {};
-    state[property] = value;
-    this.setState(state);
-  }
-  submitForm = () => {
-    console.log(this.state);
-  };
-
-  render() {
-    return (
-      <form>
-        <input
-          onChange={(ev) => {
-            this.syncChanges(ev.target.value, "email");
-          }}
-          type="email"
-          placeholder="Email"
-          value={this.state.email}
-        />
-        <input
-          onChange={(ev) => {
-            this.syncChanges(ev.target.value, "password");
-          }}
-          type="password"
-          placeholder="****"
-          value={this.state.password}
-        />
-        <div>
-          <input onClick={this.submitForm} type="submit" value="Log In" />
-        </div>
-      </form>
-    );
-  }
-}
-
-class Contador extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      contador: 0,
-    };
-  }
-
-  aumentar = () => {
-    this.setState({ contador: this.state.contador + 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.contador}</p>
-        <button onClick={this.aumentar}>Aumentar</button>
-      </div>
-    );
-  }
-}
+import Contador from './counter'
 
 class Appo extends Component {
   constructor() {
@@ -104,13 +14,12 @@ class Appo extends Component {
       name: "React",
     };
   }
+
   render() {
     let name = "Galahad";
     return (
       <div>
-        {/* <Contador></Contador>
-        <Formulario></Formulario>
-        <Blog></Blog> */}
+        <Contador />
       </div>
     );
   }
